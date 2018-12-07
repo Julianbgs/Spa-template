d58335bb9a716fa600709cd8042ed103c9f681d9
Blagis <julianbgs96@gmail.com>
2018-12-07 18:58:49 +0300

    finish project

--- public/js/script.js
+++ public/js/script.js
@@ -173,4 +173,11 @@ $(document).ready(function () {
         }
     });
 
+    //preloader
+    $(window).on('load', function () {
+        $preloader = $('.loaderArea'),
+            $loader = $preloader.find('.loader');
+        $loader.fadeOut();
+        $preloader.delay(350).fadeOut('slow');
+    });
 });
