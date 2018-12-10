a318efa5f7e4ff040ac2e132115da02a5ea5a969
Julian <julianbgs96@gmail.com>
2018-12-10 15:47:20 +0300

    change

--- public/js/script.js
+++ public/js/script.js
@@ -34,14 +34,16 @@ $(document).ready(function () {
                 breakpoint: 600,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1
+                    slidesToScroll: 1,
+                    arrows: false
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1
+                    slidesToScroll: 1,
+                    arrows: false
                 }
             }
         ]
